9c07971facfb31677191999f55a007a079679909
Feeeeddmmmeee <wislocki.janek@gmail.com>
2023-03-25 21:42:56 +0100

    updated the cnvim script

---
+++ .config/nvim/templates/cpp_bits_additional_defines.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pll pair<ll, ll>
+#define pi pair<int, int>
+#define fi first
+#define se second
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pi>
+#define vpll vector<pll>
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cout.tie(0); cin.tie(0);
+
+	
+}
