fc1cb0ed6a1f87483e2aeba2c5c02df32e5d628f
Feeeeddmmmeee <wislocki.janek@gmail.com>
2023-03-25 17:19:56 +0100

    added a new cpp template

---
+++ .config/nvim/templates/cpp_bits_using.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using llPair = pair<ll, ll>;
+using iPair = pair<int, int>;
+using iv = vector<int>;
+using llv = vector<ll>;
+using ipv = vector<iPair>;
+using llpv = vector<llPair>;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cout.tie(0); cin.tie(0);
+
+	
+}
